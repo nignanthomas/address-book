44bc56e00f18a2812cae8a39cf1669389ec9e82a
nignanthomas <nignanthomas@gmail.com>
2018-09-26 17:10:46 +0300

    download repo from nya-KE

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@
 function Contact(first,last) {
     this.firstName = first;
     this.lastName = last;
+    this.addresses = [];
+}
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
 }
 
 //User Interface Logic
@@ -9,11 +13,18 @@ $(document).ready(function() {
     $("form#new-contact").submit(function(event){
       event.preventDefault();
       var inputtedFirstName = $("input#new-first-name").val()
-      var inputtedLastName = $("input#new-Last-name").val()
+      var inputtedLastName = $("input#new-last-name").val()
 
       var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+      $("ul#contacts").append("<li><span class='contact'> " + newContact.fullName() + " </span></li>");
+
+      $(".contact").last().click(function() {
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.firstName);
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+      });
 
       $("input#new-first-name").val("");
       $("input#new-last-name").val("");
